f05d8b5daad19a709f2ed08eea3334702a9aaca1
Laura94-pixel <cristi2094@hotmail.com>
2020-08-26 12:06:35 +1000

    cambios de app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ const [algo, setAlgo] = useState({ firstName: '' })
+
+    return (
+      <div className="App">
+        <div>
+        <label >Escribe algo</label>
+        <input 
+         onChange={e => setAlgo ({escribe: e.target.value}) }
+         className = "form-control" 
+          type = "text" 
+          name = "escribe"
+          value={algo.escribe}
+          />
+        </div>
+        <div className="box" >
+        <p> {algo.escribe} </p>
+        
+        </div>
+      </div>
   );
 }
+
 
 export default App;
