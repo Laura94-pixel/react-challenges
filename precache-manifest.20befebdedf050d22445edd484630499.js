2b8edd4436f226aeb63aa97d543b93c63819e782
Laura94-pixel <cristi2094@hotmail.com>
2020-08-26 11:18:29 +1000

    Updates

---
+++ precache-manifest.20befebdedf050d22445edd484630499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b324c6564fcaafd1ce5dab035d66f3",
+    "url": "/challenges-react/index.html"
+  },
+  {
+    "revision": "3ac9880d67ccff3a06ad",
+    "url": "/challenges-react/static/css/main.b7ee4461.chunk.css"
+  },
+  {
+    "revision": "ad99b585f8435d589fa6",
+    "url": "/challenges-react/static/js/2.fa899dfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/challenges-react/static/js/2.fa899dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ac9880d67ccff3a06ad",
+    "url": "/challenges-react/static/js/main.e3b7e1d4.chunk.js"
+  },
+  {
+    "revision": "9ce18d891536e26991d6",
+    "url": "/challenges-react/static/js/runtime-main.20d56871.js"
+  }
+]);
